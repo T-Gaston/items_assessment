57b43216b9ce1641a5c1eb359ced94d227340b86
Thomas Gaston <thomasgaston11@gmail.com>
2020-04-13 11:14:13 -0600

    initial commit, quick build out of item controller completed

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,22 @@
+class Api::ItemsController < ApplicationController
+
+  def index
+    render json: Item.all
+  end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      render json: { errors: item.errors }, status: 409  
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :description, :likes)
+  end
+
+end
